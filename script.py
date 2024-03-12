b071c8639554ab5b68bb5f97bc6797868bce6be5
Dante Azucar <skynet8831@gmail.com>
2024-03-12 20:41:38 +0000

    not done yet

--- script.py
+++ script.py
@@ -1,1 +1,32 @@
+import random
+
+playernum = input("Choose a Number Between 1 and 36")
+playercolor = input("Choose a color Black(37) or Red(38)")
+
+def generate():
+    generatednum = random.randint(1,36)
+    generatedcolor = random.randint(37, 38)
+    
+    
+
+def system(playercolor,playernum,generatednum,generatedcolor):
+    if (playercolor == generatedcolor) and (playernum == generatednum):
+        print("you win")
+    else:
+        print("you lose")
+
+    system()
+    
+
+
+    
+
+
+
+    
+
+
+
+
+
 
