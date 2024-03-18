eefa6ac40b65501f2b54290ac76448e211ecda7c
Dante Azucar <skynet8831@gmail.com>
2024-03-18 20:36:08 +0000

    done with main

--- script.py
+++ script.py
@@ -1,28 +1,26 @@
 import random
 
-playernum = input("Choose a Number Between 1 and 36")
-playercolor = input("Choose a color Black(37) or Red(38)")
+playernum = int(input("Choose a Number Between 1 and 36"))
+playercolor = int(input("Choose a color Black(37) or Red(38)"))
 
 def generate():
     generatednum = random.randint(1,1)
     generatedcolor = random.randint(3, 3)
-    print(generatednum)
-    print(generatedcolor)
-    
-    
+    return generatednum, generatedcolor
     
 
 def system(playercolor,playernum,generatednum,generatedcolor):
-    if (playernum == generatednum) and (playercolor == generatedcolor):
+    if playernum == generatednum and playercolor == generatedcolor:
         print("you win")
 
     else:
         print("you lose")
     
+    
 
 
-generate()
-system('playercolor','playernum','generatednum','generatedcolor')
+generatednum, generatedcolor = generate()
+system(playercolor,playernum,generatednum,generatedcolor)
     
 
 
