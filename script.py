148e3d12c264c59e07bd5385632941c52254b8ec
Dante Azucar <skynet8831@gmail.com>
2024-03-26 16:23:21 +0000

    done

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import random
+import math
 print("you have $50 and you go to a roulette table")
-choice = input("Do you Want Play?")
+choice = input("Do you Want To Play?")
 playernum = int()
 playercolor = ("")
 moneytotal = 50
@@ -18,7 +19,7 @@ def generate():
 def system(playercolor,playernum,generatednum,generatedcolor,choice,moneynum,moneytotal,betchoice,turns):
     if moneytotal > 0:
         if choice == "yes" or choice == "Yes":
-            betchoice = int(input("Do you want to bet color & number(1) or just color(2)"))
+            betchoice = int(input("Do you want to bet color & number(1), or just color(2)"))
             moneynum = int(input("How much do you want to bet?"))
             moneytotal = moneytotal - moneynum
        
@@ -29,12 +30,12 @@ def system(playercolor,playernum,generatednum,generatedcolor,choice,moneynum,mon
                 if playernum == generatednum and playercolor == generatedcolor:
                     print("you win")
                     print("it was", generatednum,generatedcolor)
-                    moneytotal = moneytotal + 2*moneynum
+                    moneytotal = math.ceil(moneytotal + 2*moneynum)
                     print("your new total is", moneytotal)
                     print("$",moneytotal)
 
                 if playercolor == generatedcolor:
-                    moneytotal = moneytotal + moneynum/2
+                    moneytotal = math.floor(moneytotal + moneynum/2)
                     print("you only got the color right")
                     print("your new total is","$", moneytotal)
                 else:
@@ -43,9 +44,9 @@ def system(playercolor,playernum,generatednum,generatedcolor,choice,moneynum,mon
                     print("your new total is","$", moneytotal)
 
             if betchoice == 2:
-                playercolor = (input("Choose a color Black or Red"))
+                playercolor = (input("Choose a color, Black or Red"))
                 if playercolor == generatedcolor:
-                    moneytotal = moneytotal + 1.5*moneynum
+                    moneytotal = math.ceil(moneytotal + 1.5*moneynum)
                     print("you got it right")
                     print("it was" ,generatedcolor)
                     print("your new total is","$", moneytotal)
@@ -55,7 +56,7 @@ def system(playercolor,playernum,generatednum,generatedcolor,choice,moneynum,mon
                     print("your new total is","$", moneytotal)
 
 
-            choice = input("Do you Want Play Again?")
+            choice = input("Do you Want Play To Again?")
             if choice == "yes" or choice == "Yes":
                 turns.append(generatednum)
                 turns.append(generatedcolor)
@@ -64,9 +65,9 @@ def system(playercolor,playernum,generatednum,generatedcolor,choice,moneynum,mon
                 system(playercolor,playernum,generatednum,generatedcolor,choice,moneynum,moneytotal,betchoice,turns)
             else:
                 print("Thank you for playing")
-                print("*you walk out the casino*")
+                print("*you walk out the casino with*","$",moneytotal)
         else:
-            print("Loser")
+            print("*you exit the casino*")
         
     else:
         print("you have been kicked out from the casino for trying to play without money.")
